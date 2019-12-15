66adc66708bdaae52d8ad86b794325a69093c1ca
Andriichepurko <chepurko.andreyooops@gmail.com>
2019-12-15 22:14:27 +0200

    'tasks'

---
+++ .tmp_versions/chrdev.mod
@@ -0,0 +1,3 @@
+/home/andrei4ik/AndrX0917/chrdev.ko
+/home/andrei4ik/AndrX0917/chrdev.o
+
---
+++ task2/folder1/bash.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd /home/andrei4ik/AndrX0917/bash/folder2
+tar -cf file.tar MyFile
+cp file.tar /home/andrei4ik/AndrX0917/bash/folder1
+rm -rf /home/andrei4ik/AndrX0917/bash/folder1/file.tar
+
